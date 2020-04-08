79b23edcb89ad79d25af93531a4958e279a538b5
Filip Petrusevski <63159948+Filip-Shabo@users.noreply.github.com>
2020-04-08 19:28:35 +0200

    grades list added

--- Student.java
+++ Student.java
@@ -3,6 +3,17 @@ class Student {
 	String firstName;
 	String lastName;
 
+	public final List<Integer> grades;
+	public GradesList(int... numbers){
+		grades=new ArrayList<Integers>();
+		
+		for(int i:numbers){
+			grades.add(i);
+		}
+	}
+	
+
+
 	//TODO constructor
 
 	//TODO seters & getters
